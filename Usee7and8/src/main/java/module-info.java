24c21d2fc36c4fe60127fc2dc39040d9d8ffaf57
Maria-Jahan-Noon <2222637@iub.edu.bd>
2024-07-16 20:31:29 +0600

    Delete Usee7and8/src/main directory

--- Usee7and8/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.movieproduction.usee7and8 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.movieproduction.usee7and8 to javafx.fxml;
-    exports com.example.movieproduction.usee7and8;
-}
